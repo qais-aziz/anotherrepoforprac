57c1b01853331d3b595db026cd10ef8b3e65b46e
Qais Aziz <azizqais96@gmail.com>
2022-08-16 12:16:43 +0500

    3rd commit

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ const res = require('express/lib/response');
 let app = express();
 
 app.get('/', function (req, res) {
-    res.sendFile(/view/index.html)
+    res.sendFile(/views/index.html)
 })
 console.log("Hello World")
 
