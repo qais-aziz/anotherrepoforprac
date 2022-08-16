a1132e5a7ea665695c56762a98101f16b9e2ba56
Qais Aziz <azizqais96@gmail.com>
2022-08-16 12:06:31 +0500

    2nd commit

--- myApp.js
+++ myApp.js
@@ -1,5 +1,10 @@
 let express = require('express');
+const res = require('express/lib/response');
 let app = express();
+
+app.get('/', function (req, res) {
+    res.send("Hello World")
+})
 console.log("Hello World")
 
 
