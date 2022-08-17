bcfd06f89011f77801479fcdc1d49f376b728ffb
Qais Aziz <azizqais96@gmail.com>
2022-08-17 10:20:33 +0500

    5th commit

--- myApp.js
+++ myApp.js
@@ -4,6 +4,11 @@ let app = express();
 absolutePathCSS = __dirname + '/public';
 app.use("/public", express.static(absolutePathCSS));
 absolutePath = __dirname + '/views/index.html';
+app.get('/json', function(req, res){
+    res.json({
+        "message": "Hello json"
+    })
+})
 app.get('/public', function (req, res) {
     res.sendFile(absolutePath)
 })
